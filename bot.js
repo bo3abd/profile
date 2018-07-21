c68dae028026af4a4b67edca2997114dc21d62ae
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-07-21 18:07:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,22 +7,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const client = new Discord.Client();
-const fs = require('fs');
-const moment = require('moment');
-
-
-/////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`in ${client.guilds.size} servers `)
